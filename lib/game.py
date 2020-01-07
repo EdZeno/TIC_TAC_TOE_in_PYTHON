e7e3c82223829d2d3c9fd08aa39b178c32d21f34
EdZeno <eduard.kulcsickij.gut@gmail.com>
2020-01-07 10:15:38 +0000

    added a method to display the board in the terminal

--- lib/game.py
+++ lib/game.py
@@ -3,4 +3,9 @@ class Game():
              [' ',' ',' '],
              [' ',' ',' ']]
 
-    
+    def display_board(self):
+      board_copy = self.board.copy()
+      board_copy.insert(1, ['– – – – -'])
+      board_copy.insert(3, ['– – – – -'])
+      pretty_board = ''.join('\n'.join([' | '.join([str(elem) for elem in list]) for list in board_copy]))
+      return pretty_board
--- tests/test_game.py
+++ tests/test_game.py
@@ -5,3 +5,7 @@ def test_game_has_board():
     assert game.board == [[' ',' ',' '],
                           [' ',' ',' '],
                           [' ',' ',' ']]
+
+def test_display_board():
+    game = Game()
+    assert game.display_board() == '  |   |  \n–\xa0–\xa0–\xa0– -\n  |   |  \n–\xa0–\xa0–\xa0– -\n  |   |  '
