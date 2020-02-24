3aa7de34bb2e14cc8fc93249f04f15eb6d727334
EdZeno <eduard.kulcsickij.gut@gmail.com>
2020-02-24 13:39:53 +0000

    updated diplay_board method and play_game.py

--- lib/game.py
+++ lib/game.py
@@ -12,7 +12,7 @@ class Game():
       board_copy.insert(1, ['– – – – -'])
       board_copy.insert(3, ['– – – – -'])
       pretty_board = ''.join('\n'.join([' | '.join([str(elem) for elem in list]) for list in board_copy]))
-      return print(pretty_board)
+      return pretty_board
 
     def human_moves(self):
       row = input("Drawing an X\nSelect a row  1-3 (from top to buttom)  ")
--- play_game.py
+++ play_game.py
@@ -1,10 +1,10 @@
 from lib.game import Game
 
-# game = Game()
-# game.display_board()
-# game.human_moves()
-# print('Mighty AI makes the best possible move')
-# possible_moves = game.empty_cells()
-# ai_choice = game.get_best_move(game.board, possible_moves, game.turns, points=[])
-# game.ai_moves(ai_choice)
-# game.display_board()
+game = Game()
+print(game.display_board())
+game.human_moves()
+print('Mighty AI makes the best possible move')
+possible_moves = game.empty_cells()
+ai_choice = game.get_best_move(game.board, possible_moves, game.turns, points=[])
+game.ai_moves(ai_choice)
+print(game.display_board())
